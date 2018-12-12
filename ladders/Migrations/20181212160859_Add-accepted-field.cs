9220e0c7ae52dbef4e96e6000dd81131ab8312cd
Jack Thomson <Jackathomson@outlook.com>
2018-12-12 16:14:13 +0000

    Add Accepted field

---
+++ ladders/Migrations/20181212160859_Add-accepted-field.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ladders.Migrations
+{
+    public partial class Addacceptedfield : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Accepted",
+                table: "Challenge",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Accepted",
+                table: "Challenge");
+        }
+    }
+}
