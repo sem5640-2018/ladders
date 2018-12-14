93ed4c4702eb2d7818df3d0f3c05d239931b800d
James H Britton <jhb15@aber.ac.uk>
2018-12-14 01:15:15 +0000

    Partially implemented ladders repository.

--- ladders/Repositorys/LadderRepository.cs
+++ ladders/Repositorys/LadderRepository.cs
@@ -2,39 +2,53 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using ladders.Models;
 using ladders.Repositorys.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace ladders.Repositorys
 {
     public class LadderRepository : ILaddersRepository
     {
-        public Task<LadderModel> FindByIdAsync(int id)
+        private readonly LaddersContext _context;
+
+        public LadderRepository(LaddersContext context)
         {
-            throw new System.NotImplementedException();
+            _context = context;
+        }
+        
+        public async Task<LadderModel> FindByIdAsync(int id)
+        {
+            return await _context.LadderModel.FindAsync(id);
         }
 
         public Task<LadderModel> GetByIdIncAsync(int id)
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException(); //TODO Implement
         }
 
-        public Task<List<LadderModel>> GetAllAsync()
+        public async Task<List<LadderModel>> GetAllAsync()
         {
-            throw new System.NotImplementedException();
+            return await _context.LadderModel.ToListAsync();
         }
 
-        public Task<LadderModel> AddAsync(LadderModel ladder)
+        public async Task<LadderModel> AddAsync(LadderModel ladder)
         {
-            throw new System.NotImplementedException();
+            _context.LadderModel.Add(ladder);
+            await _context.SaveChangesAsync();
+            return ladder;
         }
 
-        public Task<LadderModel> UpdateAsync(LadderModel ladder)
+        public async Task<LadderModel> UpdateAsync(LadderModel ladder)
         {
-            throw new System.NotImplementedException();
+            _context.LadderModel.Update(ladder);
+            await _context.SaveChangesAsync();
+            return ladder;
         }
 
-        public Task<LadderModel> DeleteAsync(LadderModel ladder)
+        public async Task<LadderModel> DeleteAsync(LadderModel ladder)
         {
-            throw new System.NotImplementedException();
+            _context.LadderModel.Remove(ladder);
+            await _context.SaveChangesAsync();
+            return ladder;
         }
     }
 }
