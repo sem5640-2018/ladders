294fb6c350c2b0c152c250fd1682e58b9401a9de
Jack Thomson <Jackathomson@outlook.com>
2018-12-15 16:46:55 +0000

    Fix Conceding challenges

--- ladders/Data/LaddersContext.cs
+++ ladders/Data/LaddersContext.cs
@@ -16,5 +16,13 @@ namespace ladders.Models
         public DbSet<Challenge> Challenge { get; set; }
         
         public DbSet<Booking> Booking { get; set; }
+
+        public DbSet<Venue> Venue { get; set; }
+
+        public DbSet<Facility> Facility { get; set; }
+
+        public DbSet<Ranking> Ranking { get; set; }
+
+        public DbSet<Sport> Sport { get; set; }
     }
 }
