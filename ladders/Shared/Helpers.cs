b182f2ea6ca89c932c5b5eca42eaec90f3b01baf
Jack Thomson <Jackathomson@outlook.com>
2018-12-15 15:23:40 +0000

    Resolve jQuery issue when booking a venue

--- ladders/Shared/Helpers.cs
+++ ladders/Shared/Helpers.cs
@@ -37,7 +37,7 @@ namespace ladders.Shared
 
         public static async Task<IEnumerable<Venue>> GetVenues(string bookingBaseUrl, IApiClient apiClient)
         {
-            var venueData = await apiClient.GetAsync($"{bookingBaseUrl}api/facilities");
+            var venueData = await apiClient.GetAsync($"{bookingBaseUrl}api/venues");
 
             if (!venueData.IsSuccessStatusCode) return null;
 
