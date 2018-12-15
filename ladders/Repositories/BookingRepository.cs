e485b832079ab61fe76370eb0245a3f391159c4d
Jack Thomson <Jackathomson@outlook.com>
2018-12-15 17:18:49 +0000

    Resolve key error with bookings

--- ladders/Repositories/BookingRepository.cs
+++ ladders/Repositories/BookingRepository.cs
@@ -33,33 +33,9 @@ namespace ladders.Repositories
 
         public async Task<Booking> AddAsync(Booking booking)
         {
-            await SafeAddExtras(booking);
-
-            await _context.Booking.AddAsync(booking);
+            _context.Booking.Update(booking);
             await _context.SaveChangesAsync();
             return booking;
-        }
-
-        private async Task SafeAddExtras(Booking booking)
-        {
-            var currentSport = await _context.Sport.FirstOrDefaultAsync(s => s.sportId == booking.facility.sportId);
-            if (currentSport == null)
-                await _context.Sport.AddAsync(booking.facility.sport);
-            else
-                _context.Sport.Update(booking.facility.sport);
-
-            var currentVenue = await _context.Venue.FirstOrDefaultAsync(v => v.venueId == booking.facility.venueId);
-            if (currentVenue == null)
-                await _context.Venue.AddAsync(booking.facility.venue);
-            else
-                _context.Venue.Update(booking.facility.venue);
-
-            var currentFacility =
-                await _context.Facility.FirstOrDefaultAsync(b => b.facilityId == booking.facility.facilityId);
-            if (currentFacility == null)
-                await _context.Facility.AddAsync(booking.facility);
-            else
-                _context.Facility.Update(booking.facility);
         }
 
         public async Task<Booking> UpdateAsync(Booking booking)
